f6f5f49ffa104c666a62d0012cbe36b664409d67
Juha Paananen <juha.paananen@gmail.com>
2020-10-15 20:12:07 +0300

    v0.1.3

--- dist/frompromise.d.ts
+++ dist/frompromise.d.ts
@@ -12,5 +12,5 @@ export declare type PromiseRejected = {
     state: "rejected";
     error: Error;
 };
-export declare function fromPromise<I, O>(promise: Promise<I>, mapper: PromiseMapper<I, O>): Property<O>;
+export declare function fromPromise<I, O>(promise: Promise<I>, ...mapper: PromiseMapper<I, O>): Property<O>;
 export declare function fromPromise<I>(promise: Promise<I>): Property<PromiseState<I>>;
--- dist/frompromise.js
+++ dist/frompromise.js
@@ -6,7 +6,11 @@ var map_1 = require("./map");
 var property_1 = require("./property");
 var scope_1 = require("./scope");
 var tostring_1 = require("./tostring");
-function fromPromise(promise, mapper) {
+function fromPromise(promise) {
+    var mapper = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        mapper[_i - 1] = arguments[_i];
+    }
     var currentState = { state: "pending" };
     promise.then(function (value) {
         currentState = { state: "resolved", value: value };
@@ -38,7 +42,7 @@ function fromPromise(promise, mapper) {
     };
     var get = function () { return currentState; };
     var property = new property_1.StatelessProperty("fromPromise(" + tostring_1.toString(promise) + ")", get, onChange, scope_1.globalScope);
-    if (mapper) {
+    if (mapper.length > 0) {
         return map_1.map(property, function (state) {
             if (state.state === "pending")
                 return mapper[0]();
