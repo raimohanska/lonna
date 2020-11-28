525447d55e21a601b2fb8614f2f453a318244ba9
Juha Paananen <juha.paananen@gmail.com>
2020-11-28 12:39:41 +0200

    import optics-ts as proper dependency, as conditional importing doesn't really seem to work (when building applications with parcel)

--- src/lenslike.ts
+++ src/lenslike.ts
@@ -1,7 +1,7 @@
-import * as Optics from "optics-ts"
+import * as O from "optics-ts"
 import * as L from "./lens"
 
-export type LensLike<A, B> = L.Lens<A, B> | Optics.Lens<A, any, B> | Optics.Equivalence<A, any, B> | Optics.Iso<A, any, B>
+export type LensLike<A, B> = L.Lens<A, B> | O.Lens<A, any, B> | O.Equivalence<A, any, B> | O.Iso<A, any, B>
 
 const supportedOpticsTags = ["Lens", "Equivalence", "Iso"]
 
@@ -13,7 +13,7 @@ export function mkLens<A, B>(lens: LensLike<A, B>): L.Lens<A, B> {
         if (!supportedOpticsTags.includes(tag)) {
             throw Error(`Only ${supportedOpticsTags.join("/")} optics supported from optics-ts. Given optic is a ${tag}.`)
         }
-        const opticsLens = lens as Optics.Lens<A, any, B>
+        const opticsLens = lens as O.Lens<A, any, B>
         return {
             get: O.get(opticsLens),
             set: (current: A, data: B) => O.set(opticsLens)(data)(current)
@@ -21,18 +21,3 @@ export function mkLens<A, B>(lens: LensLike<A, B>): L.Lens<A, B> {
     }
     return lens as L.Lens<A, B>
 }
-
-const opticsMissing = () => { throw Error("Failed to import optics-ts. Make sure to have the optics-ts package installed.") }
-let O: typeof Optics = {
-    get: opticsMissing,
-    set: opticsMissing
-} as any
-
-async function importOptics() {
-    try {
-        O = await import("optics-ts")
-    } catch (e) {
-        
-    }
-}
-importOptics()
